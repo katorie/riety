de91d8efcd95ce8c42297048f62947d89f07649f
Takahiro Uchiyama <hochweit728@gmail.com>
2015-11-03 12:02:25 +0900

    Add shiritori script

---
+++ lib/riety/handlers/shiritori.rb
@@ -0,0 +1,17 @@
+module Riety
+  module Handlers
+    class Shiritori < Ruboty::Handlers::Base
+      on /shiritori/, name: 'shiritori', description: 'しりとり'
+
+      def shiritori(message)
+        #FIXME: 引数の一番最後の文字を綺麗に取得する方法がわからなかった
+        shiri = message.original[:body][-1]
+        if shiri == 'り'
+          message.reply '理論'
+        else
+          message.reply 'えーそんな言葉わかんない...'
+        end
+      end
+    end
+  end
+end
