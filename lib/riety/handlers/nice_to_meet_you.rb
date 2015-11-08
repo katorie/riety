8b8901a602553e581e49df0dcbd89c0601c57ddf
ta1kt0me <p.wadachi@gmail.com>
2015-11-08 17:38:36 +0900

    Welcome message to users who remarks first time

--- lib/riety/handlers/nice_to_meet_you.rb
+++ lib/riety/handlers/nice_to_meet_you.rb
@@ -4,8 +4,14 @@ module Riety
       on //, all: true, name: 'nice_to_meet_you', description: 'Welcome message'
 
       def nice_to_meet_you(message)
-        # TODO: save user who already nice to meet you
-        message.reply "Nice to meet #{message.from_name}"
+        brain = message.robot.brain
+
+        brain.data[:customers] ||= []
+
+        unless brain.data[:customers].include?(message.from) || message.from_name == ENV['ROBOT_NAME']
+          message.reply "Nice to meet #{message.from_name}"
+          brain.data[:customers] << message.from
+        end
       end
     end
   end
