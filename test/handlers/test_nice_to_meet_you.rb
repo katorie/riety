fe451b5e029934d2a49b23dccd8a561930cba2c8
ta1kt0me <p.wadachi@gmail.com>
2015-11-12 02:27:49 +0900

    Add test nice_to_meet_you

---
+++ test/handlers/test_nice_to_meet_you.rb
@@ -0,0 +1,15 @@
+require './test/test_helper'
+
+class NiceToMeetYouTest < Minitest::Test
+  def setup
+    @bot = ::Ruboty::Robot.new
+    @to = '#general'
+    @from = 'alice'
+    @said = 'hi'
+  end
+
+  def test_nice_to_meet_you
+    assert_output(/^Nice to meet #{@from}$/) { @bot.receive body: @said, from: @from, to: @to }
+    assert_silent { @bot.receive body: @said, from: @from, to: @to }
+  end
+end
