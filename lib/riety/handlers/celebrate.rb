d72e7f204d1e4ff2bdcb5ce11d2e35e21221f02f
kitamura <y.kitamura78@gmail.com>
2015-11-28 17:47:40 +0900

    Add member list option

--- lib/riety/handlers/celebrate.rb
+++ lib/riety/handlers/celebrate.rb
@@ -1,11 +1,12 @@
 module Riety
   module Handlers
     class Celebrate < Ruboty::Handlers::Base
-      on /celebrate/, name: 'celebrate', description: "みんなからのお祝い(例： #{ENV['ROBOT_NAME']} celebrate きたむら)"
+      on /celebrate/, name: 'celebrate', description: "みんなからのお祝い(例： #{ENV['ROBOT_NAME']} celebrate きたむら) | 名簿を見たかったら list オプション"
 
       def celebrate(message)
         name = message.body.split(' ')[2]
         return message.reply '名前を指定してください＼(^o^)／' unless name
+        return message.reply member_list if name == 'list'
 
         msg = get_massage_from(who: name)
         if msg
@@ -16,6 +17,9 @@ module Riety
       end
 
       private
+        def member_list
+          YAML.load_file(file_path).keys
+        end
 
         def get_massage_from(who: 'サンプル')
           @messages ||= YAML.load_file(file_path)
