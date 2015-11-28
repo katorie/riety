2d5e01711313101e6c004dcd8eec653279b8ca1b
ta1kt0me <p.wadachi@gmail.com>
2015-11-28 17:33:32 +0900

    Refactor

--- lib/riety/actions/nice_to_meet_you.rb
+++ lib/riety/actions/nice_to_meet_you.rb
@@ -1,24 +1,40 @@
 module Riety
   module Actions
     class NiceToMeetYou < Ruboty::Actions::Base
+      def initialize(message)
+        super
+        @brain ||= message.robot.brain
+        @brain.data[:customers] ||= []
+      end
+
       def call
-        brain = message.robot.brain
+        return unless first_timer?
 
-        brain.data[:customers] ||= []
+        nice_to_meet_you
+        @brain.data[:customers] << message.from
+      end
 
-        unless brain.data[:customers].include?(message.from) || message.from_name == ENV['ROBOT_NAME']
-          if message.from_name == 'katorie'
-            k = <<-EOF
+      private
+
+      def nice_to_meet_you
+        case message.from_name
+        when 'katorie'
+          res = <<-EOF
 @#{message.from_name}
 <span class="fa fa-spin">:bouquet:</span><span class="fa fa-spin">:christmas_tree:</span><span class="fa fa-spin">:bell:</span>:confetti_ball: :wedding: :rocket:<span class="fa fa-spin">:bell:</span><span class="fa fa-spin">:gift_heart:</span><span class="fa fa-spin">:bouquet:</span>
-            EOF
-            # message.reply "#{message.from_name}さん、結婚おめでとうございます:couple_with_heart::couple_with_heart::couple_with_heart:"
-            message.reply k
-          else
-            message.reply "@#{message.from_name}<span class='fa fa-spin'>:hatched_chick:</span>"
-          end
-          brain.data[:customers] << message.from
+          EOF
+          message.reply res
+        else
+          message.reply "@#{message.from_name}<span class='fa fa-spin'>:hatched_chick:</span>"
         end
+      end
+
+      def first_timer?
+        !(@brain.data[:customers].include?(message.from) || robot?)
+      end
+
+      def robot?
+        message.from_name == ENV['ROBOT_NAME']
       end
     end
   end
