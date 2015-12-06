c5dff03bb7e46d3db7614558dd49a4c540377ddd
ta1kt0me <p.wadachi@gmail.com>
2015-12-06 13:26:13 +0900

    Happy ascii art

---
+++ lib/riety/handlers/ascii_art.rb
@@ -0,0 +1,30 @@
+module Riety
+  module Handlers
+    class AsciiArt < Ruboty::Handlers::Base
+      on(
+        /aa((\s|　)+(?<keyword>.+))?\z/,
+        name: 'ascii_art',
+        command: 'aa',
+        description: 'Show ascii_art aa ebi, aa doara'
+      )
+
+      def ascii_art(message)
+        message.reply data(message[:keyword])
+      end
+
+      private
+
+        def data(name)
+          File.open(file_path(name)) do |file|
+<<-"EOF"
+#{file.read}
+EOF
+          end
+        end
+
+        def file_path(name)
+          File.join(File.expand_path('../../db/ascii_art', __FILE__), "#{name}.txt")
+        end
+    end
+  end
+end
