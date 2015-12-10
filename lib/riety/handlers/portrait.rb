7ecdd220f0a4184cb9cec3fac32213418f1c1393
Hirofumi Wakasugi <baenej@gmail.com>
2015-12-10 12:21:27 +0900

    Add portrait command to display large size portrait

---
+++ lib/riety/handlers/portrait.rb
@@ -0,0 +1,20 @@
+module Riety
+  module Handlers
+    class Portrait < Ruboty::Handlers::Base
+      on(
+        /portrait(\s|　)*\z/,
+        name: 'portrait',
+        command: 'portrait',
+        description: 'Display a large size portrait'
+      )
+
+      def portrait(message)
+        image_url = "https://s3-ap-northeast-1.amazonaws.com/llio-pub/masakari-chance.png"
+        message.reply <<-EOS
+![portrait](#{image_url})"
+by __yucao24hours__ :gift_heart:
+        EOS
+      end
+    end
+  end
+end
