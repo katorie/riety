d882d310dd77477c742c75e84dd32ea6dab8398a
Hirofumi Wakasugi <baenej@gmail.com>
2015-12-12 03:09:16 +0900

    Remove unexpected double quite

--- lib/riety/handlers/portrait.rb
+++ lib/riety/handlers/portrait.rb
@@ -11,7 +11,7 @@ module Riety
       def portrait(message)
         image_url = "https://s3-ap-northeast-1.amazonaws.com/llio-pub/rie_icon_large.jpg"
         message.reply <<-EOS
-![portrait](#{image_url})"
+![portrait](#{image_url})
 by __yucao24hours__ :gift_heart:
         EOS
       end
